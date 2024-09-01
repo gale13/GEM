42e667609895d61a72627f5a933d14fcca0ceefc
鲁新钰 <luxinyu002@sina.com>
2024-09-01 19:37:15 +0800

    Update diffusion_opt.py

--- policy/diffusion_opt.py
+++ policy/diffusion_opt.py
@@ -21,17 +21,15 @@ class DiffusionOPT(BasePolicy):
     ) -> None:
         super().__init__(**kwargs)
 
-        # Initialize actor network and optimizer if provided
+        # Initialize
         if actor is not None and actor_optim is not None:
             self._actor: torch.nn.Module = actor  # Actor network
             self._actor_optim: torch.optim.Optimizer = actor_optim  # Optimizer for the actor network
             self._action_dim = action_dim  # Dimensionality of the action space
-
-        # If learning rate decay is applied, initialize learning rate schedulers for both actor and critic
         if lr_decay:
             self._actor_lr_scheduler = CosineAnnealingLR(self._actor_optim, T_max=lr_maxt, eta_min=0.)
 
-        # Initialize other parameters and configurations
+        # Initialize other parameters
         self._rew_norm = reward_normalization  # If true, normalize rewards
         self._lr_decay = lr_decay  # If true, apply learning rate decay
         self._device = device  # Device to run computations on
@@ -47,9 +45,8 @@ class DiffusionOPT(BasePolicy):
     ) -> Batch:
         # Convert batch observations to PyTorch tensors
         obs_ = to_torch(batch[input], device=self._device, dtype=torch.float32)
-        # Use actor or target actor based on provided model argument
         model_ = self._actor
-        # Feed observations through the selected model to get action logits
+        # Get action logits
         logits, hidden = model_(obs_), None
 
         acts = logits
@@ -60,13 +57,12 @@ class DiffusionOPT(BasePolicy):
 
 
     def _update(self, batch: Batch, update: bool = False) -> torch.Tensor:
-        # Compute the behavior cloning loss
         obs_ = to_torch(batch.obs, device=self._device, dtype=torch.float32)
         expert_actions = torch.Tensor([info["expert_action"] for info in batch.info]).to(self._device)
 
         bc_loss = self._actor.loss(expert_actions, obs_).mean()
 
-        if update:  # Update actor parameters if update flag is True
+        if update:  # Update actor parameters if the update flag is True
             self._actor_optim.zero_grad()  # Zero the actor optimizer's gradients
             bc_loss.backward()  # Backpropagate the loss
             self._actor_optim.step()  # Perform a step of optimization
@@ -78,9 +74,6 @@ class DiffusionOPT(BasePolicy):
             batch: Batch,
             **kwargs: Any
     ) -> Dict[str, List[float]]:
-        # Update actor network. Here, we first calculate the policy gradient (pg_loss) and
-        # behavior cloning loss (bc_loss) but we do not update the actor network yet.
-        # The overall loss is a weighted combination of policy gradient loss and behavior cloning loss.
         overall_loss = self._update(batch, update=False)
 
         self._actor_optim.zero_grad()
@@ -96,9 +89,5 @@ class GaussianNoise:
         self.sigma = sigma
 
     def generate(self, shape):
-        """
-        :param shape: Shape of the noise to generate, typically the action's shape.
-        :return: Numpy array with Gaussian noise.
-        """
         noise = np.random.normal(self.mu, self.sigma, shape)
         return noise
