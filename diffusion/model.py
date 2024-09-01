f68c18e216b131eaa61237b3ff413227ed0ca406
鲁新钰 <luxinyu002@sina.com>
2024-09-01 19:30:33 +0800

    Update model.py

--- diffusion/model.py
+++ diffusion/model.py
@@ -51,43 +51,6 @@ class LstmRNN(nn.Module):
         x = self.linear1(x)
         return x[-1]
 
-class MLP(nn.Module):
-    def __init__(
-        self,
-        state_dim,
-        action_dim,
-        hidden_dim=256,
-        t_dim=16,
-        activation='mish'
-    ):
-        super(MLP, self).__init__()
-        _act = nn.Mish if activation == 'mish' else nn.ReLU
-        self.state_mlp = nn.Sequential(
-            nn.Linear(state_dim, hidden_dim),
-            _act(),
-            nn.Linear(hidden_dim, hidden_dim)
-        )
-        self.time_mlp = nn.Sequential(
-            SinusoidalPosEmb(t_dim),
-            nn.Linear(t_dim, t_dim * 2),
-            _act(),
-            nn.Linear(t_dim * 2, t_dim),
-        )
-        self.mid_layer = nn.Sequential(
-            nn.Linear(hidden_dim + action_dim + t_dim, hidden_dim),
-            _act(),
-            nn.Linear(hidden_dim, hidden_dim),
-            _act(),
-            nn.Linear(hidden_dim, action_dim)
-        )
-
-    def forward(self, x, time, state):
-        processed_state = self.state_mlp(state)
-        t = self.time_mlp(time)
-        x = torch.cat([x, t, processed_state], dim=1)
-        x = self.mid_layer(x)
-        return x
-
 
 
   
